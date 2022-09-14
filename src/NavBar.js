289b39544b0f0e872708d888b7944a8ee591a425
chad hinds <chadrickj8@gmail.com>
2022-09-14 14:05:31 -0400

    Initial setup

---
+++ src/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <nav href="/">
+            <ul>
+                <li>
+                    <a href="/pricing">Pricing</a>
+                </li>
+                <li>
+                    <a href="/about">About</a>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default NavBar;
