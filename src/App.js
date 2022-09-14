6b9e031d28b2ba493449d20d7a6ba987de83a099
chad hinds <chadrickj8@gmail.com>
2022-09-14 14:30:21 -0400

    Mark as active

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import NavBar from './NavBar';
-import './styles.css';
+import Pricing from "./pages/Pricing"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import { Route, Router } from "react-router-dom"
+import Navbar from "./NavBar"
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-    </div>
-  );
+    <>
+      <Navbar />
+      <div className="container">
+        <Router>
+          <Route path="/" element={<Home />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/about" element={<About />} />
+        </Router>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
