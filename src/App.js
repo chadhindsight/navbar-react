4e64797e1635d04ac3a6752714a817095229abc1
chad hinds <chadrickj8@gmail.com>
2022-10-10 12:03:15 -0400

    Fix typo

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Route, Routes } from "react-router-dom"
 import Navbar from "./NavBar"
 import './styles.css'
 
-// Main stuff would be here
+// Main stuff would be here.
 function App() {
   return (
     <>
